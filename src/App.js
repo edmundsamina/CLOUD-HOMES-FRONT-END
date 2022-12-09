c0622e6ae9533854200828d12ee59c364cb6f7f3
ionarose <ionaroseuk@gmail.com>
2022-12-09 16:19:16 +0000

    low high sorting not exactly working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import Navbar from "./components/navBar/Navbar";
 import Landing from "./components/landingPage/Landing";
 import Card from "./components/card/Card";
@@ -13,6 +13,7 @@ function App() {
   const [inputData, setinputData] = useState("");
   const [searchData, setSearchData] = useState("")
   const [ results ] = useFetchSearch(searchData);
+  const [sortLowHigh, setSortLowHigh] = useState(true)
 
 
 function clickHandler(e){
@@ -21,15 +22,28 @@ function clickHandler(e){
   setHousesArray(results)
 }
 
-// function onChangeHandler(e){
-//   setinputData(e.target.value)
-//   console.log(inputData)
-// }
+
+useEffect(() => {
+if(sortLowHigh===true){
+housesArray.sort((a, b) => (a.price - b.price) )
+}
+else {
+  housesArray.sort((a, b) => (a.price - b.price) )
+}
+console.log(sortLowHigh)
+}
+, [sortLowHigh]);
+
+
   return (
     <div className="App">
       <Navbar/>
       <Landing/>
       <Search onChange={(e)=>{setinputData(e.target.value)}} onClick={clickHandler} /> 
+      <select onChange={(e)=>{setSortLowHigh(e.target.value)}}>
+        <option value={true}>Price: Lowest to Highest</option>
+        <option value={false}>Price: Highest to Lowest</option>
+      </select>
       <Card key = {uuidv4} housesArray = {housesArray}/>
     </div>
   );
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -5,41 +5,7 @@ import cardImg from "../../assets/cardimg.png";
 
 export default function Card({housesArray}) {
  
-  // console.log("housesArray: ",housesArray);
-/*
-  const housesArray = [
-    {
-      name: "The White House",
-      location: "Washington DC",
-      price: "1000000",
-      rent: false,
-      buy: false,
-      image: "https://media.architecturaldigest.com/photos/59a6c857134a14225b851902/3:4/w_2673,h_3564,c_limit/GettyImages-167852887.jpg",
-      desc: "White it may be but black is its heart",
-      id: 1
-    },
-    {
-      name: "The London Dungeon",
-      location: "Somewhere in London",
-      price: "200000",
-      rent: false,
-      buy: true,
-      image: "https://res.cloudinary.com/hello-tickets/image/upload/c_limit,f_auto,q_auto,w_768/v1644424296/post_images/london-2/The-London-Dungeon/LauraLippay-London-Dungeon.jpg",
-      desc: "Home of London's political elite",
-      id: 2
-    },
-    {
-      name: "Windmill Upon Hills",
-      location: "Genting Permai, Pahang",
-      price: "14799",
-      rent: false,
-      buy: true,
-      image: "https://apicms.thestar.com.my/uploads/images/2020/10/10/892960.jpg",
-      desc: "OSK Property Malaysia",
-      id: 3
-    }
-  ]
-*/
+
   return (
     <div className="card-index">
     {housesArray.map(({name, location, price, rent, buy, image, desc, id}) => {
@@ -56,8 +22,8 @@ export default function Card({housesArray}) {
           <p>{desc}</p>
           <p>{location}</p>
           <p className="price">${price}</p>
-          {rent ? <><input type="checkbox" checked /><label>Rent</label></> : <><input type="checkbox" /><label>Rent</label></>}
-          {buy ? <><input type="checkbox" checked /><label>Rent</label></> : <><input type="checkbox" /><label>Buy</label></>}
+          {rent ? <p className="rent">Rent</p> : <p className="buy">Buy</p>}
+          
           </div>
       );
     })}
