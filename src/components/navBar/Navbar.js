de43b396e369afa089a5076c352a199de31026ea
edmundsamina <edmundsamina@gmail.com>
2022-12-09 16:34:46 +0000

    some css added

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -4,7 +4,8 @@ import logo from '../../assets/icons8-home-app-200.png'
 export default function Navbar(){
     return(
         <nav>
-         <img src={logo} alt = "house in the clouds" className = "nav-logo"/>   
+         <img src={logo} alt = "house in the clouds" className = "nav-logo"/>
+         <h3 className='logo-name'>Cloud Homes</h3>   
         </nav>
     )
 }
