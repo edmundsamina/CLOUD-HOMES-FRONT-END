dcfd8b8e734734e273a375d5a47b336e15f6061c
edmundsamina <edmundsamina@gmail.com>
2022-12-18 16:42:38 +0000

    backend-domain changed

--- src/hooks/useFetchSearch.js
+++ src/hooks/useFetchSearch.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 
-export default function useFetchSearch(searchData) {
+export default function useFetchSearch(searchData, searchKey) {
     const [searchArray, setsearchArray] = useState([]);
 
     useEffect(() => {
-        const domain="http://localhost:3002";
+        const domain="https://cloudhomesbackend.onrender.com";
         async function getData() {
-        const response = await fetch(`${domain}/api/properties/?description=${searchData}`);
+        const response = await fetch(`${domain}/api/properties/?${searchKey}=${searchData}`);
             const data = await response.json();
             setsearchArray(data.payload);
         }
         getData();
-    }, [searchData]);
+    }, [searchData, searchKey]);
 
     return [ searchArray ];
 }
