533ac77f071ce12f0d93014d622d816657809d40
ionarose <ionaroseuk@gmail.com>
2022-12-26 23:12:07 +0000

    popups working, css skewed though so don't merge

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,16 +3,32 @@ import bedrooms from "../../assets/icons8-bed-96.png";
 import bathrooms from "../../assets/icons8-shower-96.png";
 import garden from "../../assets/icons8-tree-planting-96.png";
 import logo from "../../assets/icons8-google-96.png";
+import Modal from "../Modal/Modal.js"
+import { useState } from "react"
 
 export default function Card(props) {
+
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className="card-index">
+    <div className="card-index" >
       {/* {housesArray.map(({name, location, price, rent, buy, image, desc, id}) => {
       return( */}
       <div className="card-container">
-        <div className="card">
+        <div className="card" >
+        {showModal ? (
+          <Modal>
+            <div className ="pop-up-container">
+              <h1>{props.name}</h1>
+              
+                
+                <button onClick={() => setShowModal(false)}>Close</button>
+              </div>
+            
+          </Modal>
+        ) : null}
           <div className="image-container">
-            <img className="img" src={props.image} alt={props.name} />{" "}
+            <img onClick={() => setShowModal(true)} className="img" src={props.image} alt={props.name} />{" "}
           </div>
           <h3>{props.name.toUpperCase()}</h3>
           <div className="price-days-container">
@@ -43,12 +59,17 @@ export default function Card(props) {
               <img className="icon" src={bathrooms} alt="bathroom icon" />
               {props.bathrooms}
             </span>
-          </div>
+          
+          
+        
+        
           <footer>
             <img className="profile-icon" src={logo} alt="creator" />
             <span className="creator-subtitle">{props.metaverse} - </span>
             {props.city}
           </footer>
+          
+      </div>
         </div>
       </div>
       {/* ); */}
